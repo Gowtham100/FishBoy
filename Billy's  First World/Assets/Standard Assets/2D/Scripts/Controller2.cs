b8f5c0e611591463c2d7ba858c033ffb99c908f2
Gowtham100 <gowtham100mohan@gmail.com>
2015-11-02 17:28:30 -0900

    From Gowtham

---
+++ Billy's  First World/Assets/Standard Assets/2D/Scripts/Controller2.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller2 : MonoBehaviour {
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+	
+	Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		
+		anim = GetComponent<Animator>();
+		
+	}
+
+	void Update(){
+		bool shoot = Input.GetButton ("Fire1");
+		shoot |= Input.GetButtonDown("Fire2");
+		if (shoot)
+		{
+			Weapon weapon = GetComponent<Weapon>();
+			if (weapon != null)
+			{
+				// false because the player is not an enemy
+				weapon.Attack(false);
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	
+		
+
+		Horizontal ();
+		Vertical ();
+
+		
+	}
+	void Horizontal()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat ("Speed", Mathf.Abs(moveHorizontal));
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveHorizontal * maxSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		if (moveHorizontal  > 0 && ! facingRight)
+			Flip ();
+		else if (moveHorizontal < 0 && facingRight)
+			Flip ();
+
+
+	}
+
+	void Vertical ()
+	{
+		float moveVertical = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", Mathf.Abs (moveVertical));
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVertical * maxSpeed, GetComponent<Rigidbody2D> ().velocity.x);
+
+		if (moveVertical  > 0 && ! facingRight)
+			Flip ();
+		else if (moveVertical < 0 && facingRight)
+			Flip ();
+	}
+	
+	void Flip()
+	{
+		facingRight = ! facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
+
---
+++ Billy's  First World/Assets/Standard Assets/2D/Scripts/Weapon.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+	
+
+	public Transform shotPrefab;
+
+	public float shootingRate = 0.25f;
+		
+	private float shootCooldown;
+
+	public LayerMask notToHit;
+
+
+
+	void Start()
+	{
+		shootCooldown = 0f;	
+
+
+
+	}
+
+	void Update()
+	{
+
+
+		if (shootCooldown > 0)
+		{
+
+			shootCooldown -= Time.deltaTime;
+
+	}
+
+
+
+
+
+	}
+	public void Attack(bool isEnemy)
+	{
+		if (CanAttack)
+		{
+			shootCooldown = shootingRate;
+			
+			// Create a new shot
+			var shotTransform = Instantiate(shotPrefab) as Transform;
+			
+			// Assign position
+			shotTransform.position = transform.position;
+			
+			// The is enemy property
+			ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+			if (shot != null)
+			{
+				shot.isEnemyShot = isEnemy;
+
+			}
+			
+			// Make the weapon shot always towards it
+			MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+			if (move != null)
+			{
+				move.direction = this.transform.right; // towards in 2D space is the right of the sprite
+			
+
+			}
+		}
+	}
+
+	public bool CanAttack
+	{
+		get
+		{
+			return shootCooldown <= 0f;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
