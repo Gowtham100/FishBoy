a413299a6ebfe30fafb4eee3e200b38c770a968b
Gowtham100 <gowtham100mohan@gmail.com>
2015-11-28 14:22:08 -0900

    done

---
+++ Billy's  First World/Assets/Standard Assets/Scripts/Controller3.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller3 : MonoBehaviour {
+
+	Animator anim;
+	public float maxSpeed = 0.0005f;
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float input_x = Input.GetAxisRaw ("Horizontal");
+		float input_y = Input.GetAxisRaw ("Vertical");
+
+
+
+		bool isWalking = (Mathf.Abs (input_x) + Mathf.Abs (input_y)) > 0;
+
+		anim.SetBool ("isWalking", isWalking);
+
+		if (isWalking) {
+			anim.SetFloat("X", input_x);
+			anim.SetFloat("Y", input_y);
+
+			transform.position += new Vector3(input_x, input_y, 0).normalized * Time.deltaTime * maxSpeed;
+
+		}
+
+	
+	}
+}
