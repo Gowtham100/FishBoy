045a2e7d2ec0530af71208ec61bf1f10c062dce4
Jeanie Suparman <jeanie.wu@hotmail.com>
2015-12-27 17:58:00 -0800

    move damage to shot prefab

--- Billy's  First World/Assets/Standard Assets/Scripts/Shot.cs
+++ Billy's  First World/Assets/Standard Assets/Scripts/Shot.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class Shot : MonoBehaviour {
 
 	public float speed;
+	public int damage;
+
+	public Controller3 player;
 
 
 	// Use this for initialization
 	// this method is executed when we instantiated the laser object
 	void Start () {
+		damage = 10;
 		speed = 15;
 		// transform.forward is to make it move forward (along x axis in this case)
 		GetComponent<Rigidbody2D> ().velocity = transform.right * speed;
+
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Controller3>();
 
 	}
 	
@@ -22,8 +28,11 @@ public class Shot : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
-		if (other.gameObject.CompareTag ("Player"))
+		if (other.gameObject.CompareTag ("Player")) {
 			Destroy (gameObject);
+			player.life -= damage;
+		}
+			
 	}
 
 
